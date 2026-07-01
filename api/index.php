f0f97eb16d1c7e0d35285683556328cad0f57598
RachelDisajin <racheldisatr@gmail.com>
2026-07-01 15:50:04 +0700

    Fix Vercel serverless issues and add Vite build

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
 <?php
 
+// Vercel serverless environment adjustments
+if (isset($_ENV['VERCEL'])) {
+    $_ENV['LOG_CHANNEL'] = 'stderr';
+    $_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+    $_ENV['SESSION_DRIVER'] = 'cookie';
+    
+    if (!is_dir($_ENV['VIEW_COMPILED_PATH'])) {
+        mkdir($_ENV['VIEW_COMPILED_PATH'], 0777, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
